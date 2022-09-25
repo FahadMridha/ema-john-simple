fbb981076cab408782fd742d3dcfca96ce492aea
fahadMridha <sohelcse16@gmail.com>
2022-09-26 03:14:05 +0600

    asses localstorage

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import './Cart.css'
 
 const Cart = ({cart}) => {
+    let total=0;
+    let shipping=0;
+    for(const product of cart){
+        total=total+product.price;
+        shipping=shipping+product.shipping;
+    }
+    const tax=parseFloat((total*.1).toFixed(2));
+    const grandTotal=total+shipping+tax;
     return (
-        <div>
+        <div className='cart'>
             <h3>Order Summry</h3>
             <h5>selecet products: {cart.length} </h5>
+            <p>Total Price: ${total} </p>
+            <p> Total Shipping Charge: ${shipping}</p>
+            <p>Tax: ${tax}</p>
+            <h5>Grand Total: ${grandTotal.toFixed(2)}</h5>
         </div>
     );
 };
